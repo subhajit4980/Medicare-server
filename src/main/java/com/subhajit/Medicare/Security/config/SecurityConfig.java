9c7aef556df5588acb24a70aefb9701677a30c56
subhajitpatra498 <subhajitpatra498@gmail.com>
2024-02-05 16:19:24 +0530

    add cors

--- src/main/java/com/subhajit/Medicare/Security/config/SecurityConfig.java
+++ src/main/java/com/subhajit/Medicare/Security/config/SecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -18,11 +19,9 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.Arrays;
 import java.util.Collections;
@@ -62,41 +61,74 @@ public class SecurityConfig {
     }
 
 
+//    @Bean
+//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//
+//        http.exceptionHandling(exception -> exception.authenticationEntryPoint(point))
+//                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+//                .authorizeHttpRequests(auth -> auth
+//                        .requestMatchers("/api/auth/**").permitAll()
+//                        .requestMatchers("/api/productUser/**").permitAll()
+//                        .requestMatchers("/api/test/**").permitAll()
+//                        .anyRequest().authenticated());
+//        http.authenticationProvider(authenticationProvider());
+//        http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
+//        http.csrf(AbstractHttpConfigurer::disable).cors(cors-> new CorsConfigurationSource() {
+//            @Override
+//            public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+//                CorsConfiguration cfg=new CorsConfiguration();
+//                cfg.setAllowedOrigins(Collections.singletonList("*"));
+//                cfg.setAllowCredentials(true);
+//                cfg.setAllowedHeaders(Collections.singletonList("*"));
+//                cfg.setAllowedMethods(Collections.singletonList("*"));
+//                cfg.setExposedHeaders(Arrays.asList("Authorization"));
+//                cfg.setMaxAge(3600L);
+//                return cfg;
+//            }
+//        });
+//        return http.build();
+//    }
+// Create a CorsConfigurationSource bean that defines the allowed origins, methods, headers, and other attributes for CORS
+@Bean
+public CorsConfigurationSource corsConfigurationSource() {
+    CorsConfiguration configuration = new CorsConfiguration();
+    configuration.setAllowedOrigins(Arrays.asList("*")); // You can specify the origins you want to allow
+    configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE")); // You can specify the methods you want to allow
+    configuration.setAllowedHeaders(Arrays.asList("*")); // You can specify the headers you want to allow
+    configuration.setAllowCredentials(true); // You can specify if you want to support credentials
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", configuration); // You can specify the path pattern you want to apply the configuration
+    return source;
+}
+
+    // Create a SecurityFilterChain bean using the HttpSecurity DSL
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-
-        http.exceptionHandling(exception -> exception.authenticationEntryPoint(point))
-                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/Admin/**").hasAuthority("ADMIN") // Require ADMIN authority for admin API
-                        .anyRequest().permitAll()
-                );
+        http.cors(Customizer.withDefaults()) // Add the .cors().and() filter to the security chain
+                .csrf(Customizer.withDefaults())
+                .authorizeHttpRequests((requests) -> requests
+                        .requestMatchers(PUBLIC_URLS).permitAll() // Allow CORS option calls for Swagger UI
+                        .requestMatchers("/openapi/openapi.yml").permitAll() // Allow access to the Open API URL
+                        .anyRequest().authenticated()) // Require authentication for any other request
+                .httpBasic(Customizer.withDefaults());
         http.authenticationProvider(authenticationProvider());
         http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
-        http.csrf(AbstractHttpConfigurer::disable).cors(cors-> new CorsConfigurationSource() {
-            @Override
-            public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
-                CorsConfiguration cfg=new CorsConfiguration();
-                cfg.setAllowedOrigins(Collections.singletonList("*"));
-                cfg.setAllowCredentials(true);
-                cfg.setAllowedHeaders(Collections.singletonList("*"));
-                cfg.setAllowedMethods(Collections.singletonList("*"));
-                cfg.setExposedHeaders(Arrays.asList("Authorization"));
-                cfg.setMaxAge(3600L);
-                return cfg;
-            }
-        });
         return http.build();
     }
-    @Bean
-    public WebMvcConfigurer confg()
-    {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("*");
-                WebMvcConfigurer.super.addCorsMappings(registry);
-            }
-        };
-    }
+    public static final String[] PUBLIC_URLS = {
+            "/home",
+            "/api/auth/**",
+            "/api/productUser/**",
+            "/api/test/**",
+            "/v2/api-docs",
+            "/v3/api-docs",
+            "/v3/api-docs/**",
+            "/swagger-resources",
+            "/swagger-resources/**",
+            "/configuration/ui",
+            "/configuration/security",
+            "/swagger-ui/**",
+            "/webjars/**",
+            "/swagger-ui.html"
+    };
 }
